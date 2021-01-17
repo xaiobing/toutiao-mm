8e0fe1f83caf5b28f9b93943efb5cb449949820c
huyanjun <1353139951@qq.com>
2021-01-17 14:45:39 +0800

    导入vant

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 // 全局样式文件
 import './style/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
